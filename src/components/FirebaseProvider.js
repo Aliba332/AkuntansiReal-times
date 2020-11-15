422f3837a4ea5a0be15148b9a0346bfbaec556d2
aliba332 <marendrarojali51@gmail.com>
2020-11-15 13:24:20 +0700

    commit ke 2

--- src/components/FirebaseProvider.js
+++ src/components/FirebaseProvider.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import * as firebase from "firebase/app";
+import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
